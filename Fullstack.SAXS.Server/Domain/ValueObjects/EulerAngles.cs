7740f0017a5d746bf859833a4835afe1e47acded
denis <denis@PC_DEN4IK>
2025-06-16 22:45:15 +0300

    Added more Entities

---
+++ Fullstack.SAXS.Server/Domain/ValueObjects/EulerAngles.cs
@@ -0,0 +1,37 @@
+﻿using System.Numerics;
+
+namespace Fullstack.SAXS.Server.Domain.ValueObjects
+{
+    /// <summary>
+    /// Rotation angles:
+    /// PraecessioAngle - Alpha
+    /// NutatioAngle - Betta
+    /// ProperRotationAngle - Gamma
+    /// </summary>
+    public readonly struct EulerAngles
+    {
+        public readonly float PraecessioAngle;
+        public readonly float NutatioAngle;
+        public readonly float ProperRotationAngle;
+
+        public EulerAngles(float alpha, float betta, float gamma)
+        {
+            PraecessioAngle = alpha;
+            NutatioAngle = betta;
+            ProperRotationAngle = gamma;
+        }
+
+        public Matrix4x4 CreateRotationMatrix()
+        {
+            float alpha = MathF.PI * PraecessioAngle / 180.0f;
+            float beta = MathF.PI * NutatioAngle / 180.0f;
+            float gamma = MathF.PI * ProperRotationAngle / 180.0f;
+
+            var rotationX = Matrix4x4.CreateRotationX(alpha);
+            var rotationY = Matrix4x4.CreateRotationY(beta);
+            var rotationZ = Matrix4x4.CreateRotationZ(gamma);
+
+            return rotationX * rotationY * rotationZ;
+        }
+    }
+}
