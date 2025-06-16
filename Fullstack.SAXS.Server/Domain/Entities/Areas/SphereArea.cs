84cc482a051152ae471ad9ce3eb8c4dc3c0b4114
denis <denis@PC_DEN4IK>
2025-06-16 22:43:47 +0300

    Added Areas

---
+++ Fullstack.SAXS.Server/Domain/Entities/Areas/SphereArea.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+using Fullstack.SAXS.Server.Application.Interfaces;
+using Fullstack.SAXS.Server.Domain.Entities.Particles;
+using Microsoft.AspNetCore.Components.Routing;
+
+namespace Fullstack.SAXS.Server.Domain.Entities.Areas
+{
+    public class SphereArea(int series, float radius, IOctree<Particle> octree) : Area(series, octree)
+    {
+        public override float OuterRadius => radius;
+
+        public override bool Contains(Particle particle)
+        {
+            var rDiff = radius - particle.OuterSphereRadius;
+
+            return particle.Center.LengthSquared() <= rDiff * rDiff;
+         }
+    }
+}
