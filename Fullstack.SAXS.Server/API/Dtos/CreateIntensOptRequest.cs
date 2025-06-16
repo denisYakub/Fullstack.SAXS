be3eeb8471f780c53518eff0a811ba8b7b623c19
denis <denis@PC_DEN4IK>
2025-06-16 22:43:10 +0300

    Added Dtos request entities

---
+++ Fullstack.SAXS.Server/API/Dtos/CreateIntensOptRequest.cs
@@ -0,0 +1,21 @@
+﻿namespace Fullstack.SAXS.Server.API.Dtos
+{
+    public record struct CreateIntensOptRequest(
+        float QMin, float QMax, int QNum
+    )
+    {
+        public readonly bool isLegit
+        {
+            get
+            {
+                if (QMin > QMax)
+                    return false;
+
+                if (QNum <= 0)
+                    return false;
+
+                return true;
+            }
+        }
+    }
+}
---
+++ Fullstack.SAXS.Server/API/Dtos/CreateSysRequest.cs
@@ -0,0 +1,41 @@
+﻿namespace Fullstack.SAXS.Server.API.Dtos
+{
+    public record struct CreateSysRequest(
+        float? AreaRadius, float? Nc,
+        float ParticleMinSize, float ParticleMaxSize,
+        float ParticleAlphaRotation,
+        float ParticleBetaRotation,
+        float ParticleGammaRotation,
+        float ParticleSizeShape, float ParticleSizeScale,
+        int ParticleNumber, int AreaNumber
+    )
+    {
+        public readonly bool IsLegit
+        {
+            get
+            {
+                if (!AreaRadius.HasValue || !Nc.HasValue)
+                    return false;
+
+                if (ParticleMinSize > ParticleMaxSize)
+                    return false;
+
+                if (AreaNumber <= 0 || ParticleNumber <= 0)
+                    return false;
+
+                return true;
+            }
+        }
+
+        public readonly float AreaSize
+        {
+            get
+            {
+                if (Nc.HasValue)
+                    throw new NotImplementedException();
+
+                return AreaRadius.Value;
+            }
+        }
+    }
+}
