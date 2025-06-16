6046b1751881bc5ef998baa03c912ea4545b307b
denis <denis@PC_DEN4IK>
2025-06-16 22:44:39 +0300

    Added factories for areas and particles

---
+++ Fullstack.SAXS.Server/Infastructure/Factories/AreaParticleFactory.cs
@@ -0,0 +1,17 @@
+﻿using Fullstack.SAXS.Server.Domain.Entities.Areas;
+using Fullstack.SAXS.Server.Domain.Entities.Particles;
+
+namespace Fullstack.SAXS.Server.Infastructure.Factories
+{
+    public abstract class AreaParticleFactory
+    {
+        public abstract IEnumerable<Area> GetAreas(float areaSize, int number);
+        public abstract IEnumerable<Particle> GetInfParticles(
+            float minSize, float maxSize, float sizeShape, float sizeScale,
+            float alphaRotation, float betaRotation, float gammaRotation,
+            float minX, float maxX,
+            float minY, float maxY,
+            float minZ, float maxZ
+        );
+    }
+}
