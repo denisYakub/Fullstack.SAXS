46f7870803dbb4e9e7fc16f4643c995c03c67d90
denis <denis@PC_DEN4IK>
2025-06-16 22:42:25 +0300

    Added Extensions for gamma and random

---
+++ Fullstack.SAXS.Server/Domain/Commands/GammaExtensions.cs
@@ -0,0 +1,19 @@
+﻿using MathNet.Numerics.Distributions;
+
+namespace Fullstack.SAXS.Server.Domain.Commands
+{
+    public static class GammaExtensions
+    {
+        public static float GetGammaRandom(this Gamma gamma, float min, float max)
+        {
+            float randValue = (float)gamma.Sample();
+
+            while (randValue <= min || randValue >= max)
+            {
+                randValue = (float)gamma.Sample();
+            }
+
+            return randValue;
+        }
+    }
+}
---
+++ Fullstack.SAXS.Server/Domain/Commands/RandomExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Fullstack.SAXS.Server.Domain.Commands
+{
+    public static class RandomExtensions
+    {
+        public static float GetEvenlyRandom(this Random random, float min, float max)
+        {
+            return min + (max - min) * (float)random.NextDouble();
+        }
+    }
+}
