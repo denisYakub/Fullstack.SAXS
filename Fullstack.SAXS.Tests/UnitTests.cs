5dc1eff1b31e39d5f90edd402da73658f6198e5c
denis <denis@PC_DEN4IK>
2025-06-17 19:10:32 +0300

    added unit test for factory

---
+++ Fullstack.SAXS.Tests/UnitTests.cs
@@ -0,0 +1,54 @@
+﻿using Fullstack.SAXS.Server.Application.Interfaces;
+using Fullstack.SAXS.Server.Domain.Commands;
+using Fullstack.SAXS.Server.Domain.Entities.Areas;
+using Fullstack.SAXS.Server.Domain.Entities.Octrees;
+using Fullstack.SAXS.Server.Domain.Entities.Particles;
+using Fullstack.SAXS.Server.Domain.Entities.Regions;
+using Fullstack.SAXS.Server.Infastructure.Factories;
+using MathNet.Numerics.Distributions;
+
+namespace Fullstack.SAXS.Tests
+{
+    public class Tests
+    {
+        private int areaR = 3000, areaNumber = 3, particleNumber = 100_000;
+        private SphereIcosahedronFactory _factory;
+
+        [SetUp]
+        public void Setup()
+        {
+            _factory = new SphereIcosahedronFactory();
+        }
+
+        [Test]
+        public void Test_SphereIcosahedronFactory_Methods()
+        {
+            //Init
+            var startRegion = new Region(new(0, 0, 0), areaR * 2);
+            var octree = new Octree(startRegion.MaxDepth(3 * 5), startRegion);
+            var areas = 
+                _factory
+                .GetAreas(areaR, areaNumber, octree);
+            //Action
+            Parallel.ForEach(areas, area =>
+            {
+                var infParticles =
+                _factory
+                .GetInfParticles(
+                    1, 5,
+                    3, 2.5f,
+                    360, 360, 360,
+                    -areaR, areaR,
+                    -areaR, areaR,
+                    -areaR, areaR
+                );
+
+                area.Fill(infParticles, particleNumber);
+
+                var particles = area.Particles.ToList();
+            });
+            //Assert
+            Assert.Pass();
+        }
+    }
+}
