08307acc9f1a8772279b579d4d92c47ff6517710
denis <denis@PC_DEN4IK>
2025-06-17 19:08:51 +0300

    release version of db context

---
+++ Fullstack.SAXS.Server/Infastructure/Persistence/DbContexts/PosgresDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Fullstack.SAXS.Server.Domain.Entities.Sp;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fullstack.SAXS.Server.Infastructure.Persistence.DbContexts
+{
+    public class PosgresDbContext(DbContextOptions<PosgresDbContext> options) : IdentityDbContext(options)
+    {
+        public DbSet<SpData> Datas { get; private set; }
+        public DbSet<SpGeneration> Generations { get; private set; }
+        public DbSet<SpGenerationNumberCounter> GenerationCurrentNum { get; private set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder
+                .Entity<SpGeneration>()
+                .HasOne(gen => gen.SpData)
+                .WithOne(data => data.SpGeneration)
+                .HasForeignKey<SpGeneration>(gen => gen.IdSpData);
+        }
+    }
+}
