49468a072c0dc0f37b33a0a80f89aab6cba6e692
denis <denis@PC_DEN4IK>
2025-06-20 19:23:55 +0300

    Impl of WriteAsync

--- Fullstack.SAXS.Persistence/IO/FileService.cs
+++ Fullstack.SAXS.Persistence/IO/FileService.cs
@@ -67,9 +67,54 @@ namespace Fullstack.SAXS.Infrastructure.IO
             return path;
         }
 
-        public Task<string> WriteAsync(Area obj, string folderPath)
+        public async Task<string> WriteAsync(Area obj, string folderPath)
         {
-            throw new NotImplementedException();
+            var file = new StringBuilder();
+
+            file.AppendJoin('_', [
+                $"{nameof(obj.Series)}#{obj.Series}",
+                $"{nameof(obj.OuterRadius)}#{obj.OuterRadius}",
+                $"{nameof(obj.AreaType)}#{obj.AreaType}",
+                $"{nameof(obj.ParticlesType)}#{obj.ParticlesType}",
+            ]);
+            file.Append(".csv");
+
+            var path = Path.Combine(folderPath, file.ToString());
+
+            var prtcls = obj.Particles;
+
+            var text = new StringBuilder();
+
+            if (prtcls.Any())
+            {
+                var prtclF = prtcls.First();
+                string[] headers = [
+                    nameof(prtclF.Size),
+                    nameof(prtclF.Center),
+                    nameof(prtclF.RotationAngles),
+                ];
+
+                text.AppendJoin(";", headers);
+                text.AppendLine();
+
+                foreach (var prtcl in prtcls)
+                {
+                    string[] values = [
+                        prtcl.Size.ToString(),
+                        prtcl.Center.ToString(),
+                        prtcl.RotationAngles.ToString()
+                    ];
+
+                    text.AppendJoin(";", values);
+                    text.AppendLine();
+                }
+            }
+
+            var writeTask = File.WriteAllTextAsync(path, text.ToString(), Encoding.UTF8);
+
+            await writeTask;
+
+            return path;
         }
     }
 }
