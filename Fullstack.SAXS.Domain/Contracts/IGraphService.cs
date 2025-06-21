bffa711d5e4cd8cc22d6e2b6e96ee86d0c6abdef
denis <denis@PC_DEN4IK>
2025-06-21 13:46:48 +0300

    Added GrafService

---
+++ Fullstack.SAXS.Domain/Contracts/IGraphService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fullstack.SAXS.Domain.Contracts
+{
+    public interface IGraphService
+    {
+        Task<string> GetHtmlPage(float[] x, float[] y);
+    }
+}
---
+++ Fullstack.SAXS.Persistence/HTML/GraphService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Fullstack.SAXS.Domain.Contracts;
+
+namespace Fullstack.SAXS.Persistence.HTML
+{
+    public class GraphService(string uri) : IGraphService
+    {
+        public async Task<string> GetHtmlPage(float[] x, float[] y)
+        {
+            var request = new { x, y };
+            var json = JsonSerializer.Serialize(request);
+
+            using var client = new HttpClient();
+
+            var content = new StringContent(
+                json, 
+                Encoding.UTF8, 
+                "application/json"
+            );
+
+            var response = await client.PostAsync(uri, content);
+
+            var html = await response.Content.ReadAsStringAsync();
+
+            return html;
+        }
+    }
+}
