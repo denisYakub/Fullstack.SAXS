d31f55d91be22eb84ae1c39415cc6a4254631942
denis <denis@PC_DEN4IK>
2025-06-21 12:55:54 +0300

    added simple draw graf api endpoint

--- Fullstack.SAXS.Api/Fullstack.SAXS.Api.py
+++ Fullstack.SAXS.Api/Fullstack.SAXS.Api.py
@@ -1,3 +1,22 @@
-import matplotlib.pyplot as plt, mpld3
-plt.plot([3,1,4,1,5], 'ks-', mec='w', mew=5, ms=20)
-mpld3.show()
+from flask import Flask, request, Response
+import matplotlib.pyplot as plt
+import mpld3
+import json
+
+app = Flask(__name__)
+
+@app.route('/plot', methods=['POST'])
+def plot():
+    data = request.get_json()
+    x = data.get("x", [])
+    y = data.get("y", [])
+
+    fig, ax = plt.subplots()
+    ax.plot(x, y, marker='o')
+    ax.set_title("Your Graph")
+
+    html_str = mpld3.fig_to_html(fig)
+    return Response(html_str, mimetype='text/html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
