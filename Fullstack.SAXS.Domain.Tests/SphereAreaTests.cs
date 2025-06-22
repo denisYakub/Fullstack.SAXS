3c865c8dcabfe79074d3089fe127c0bb1ae7cff3
denis <denis@PC_DEN4IK>
2025-06-22 17:58:30 +0300

    Testing fill method in Area

---
+++ Fullstack.SAXS.Domain.Tests/SphereAreaTests.cs
@@ -0,0 +1,71 @@
+﻿using Fullstack.SAXS.Server.Domain.Commands;
+using Fullstack.SAXS.Server.Domain.Entities.Areas;
+using Fullstack.SAXS.Server.Domain.Entities.Particles;
+using MathNet.Numerics.Distributions;
+
+namespace Fullstack.SAXS.Domain.Tests
+{
+    public class ShereAreaTests
+    {
+        private IEnumerable<Particle> _infParticles;
+        private float _maxParticleSize = 5;
+        private float _areaRadius = 3000;
+
+        [SetUp]
+        public void Setup()
+        {
+            _infParticles = InfParticleGenerator();
+        }
+
+        [Test]
+        public void Fill_WithSmallAnount_AreaWith100Particles()
+        {
+            // Arrange
+            var area = new SphereArea(0, _areaRadius, _maxParticleSize);
+
+            // Act
+            area.Fill(_infParticles, 100);
+
+            // Assert
+            Console.WriteLine(area.Particles.Count());
+
+            Assert.That(area.Particles.Any());
+        }
+
+        [Test]
+        public void Fill_WithBigAnount_AreaWith1000000Particles()
+        {
+            // Arrange
+            var area = new SphereArea(0, _areaRadius, _maxParticleSize);
+
+            // Act
+            area.Fill(_infParticles, 1000000);
+
+            // Assert
+            Console.WriteLine(area.Particles.Count());
+
+            Assert.That(area.Particles.Any());
+        }
+
+        private IEnumerable<Particle> InfParticleGenerator()
+        {
+            var random = new Random();
+            var gamma = new Gamma(3, 2.5f);
+
+            while (true)
+            {
+                var size = gamma.GetGammaRandom(1, _maxParticleSize);
+
+                var a = random.GetEvenlyRandom(-360, 360);
+                var b = random.GetEvenlyRandom(-360, 360);
+                var g = random.GetEvenlyRandom(-360, 360);
+
+                var x = random.GetEvenlyRandom(-_areaRadius, _areaRadius);
+                var y = random.GetEvenlyRandom(-_areaRadius, _areaRadius);
+                var z = random.GetEvenlyRandom(-_areaRadius, _areaRadius);
+
+                yield return new IcosahedronParticle(size, new(x, y, z), new(a, b, g));
+            }
+        }
+    }
+}
