91e03e598009e44e7068d8ea8ed1dca8f81ce894
denis <denis@PC_DEN4IK>
2025-06-23 19:06:21 +0300

    More tests added

---
+++ Fullstack.SAXS.Application.Tests/SysServiceTests.cs
@@ -0,0 +1,97 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using Fullstack.SAXS.Domain.Contracts;
+using Fullstack.SAXS.Server.Domain.Commands;
+using Fullstack.SAXS.Server.Domain.Entities.Areas;
+using Fullstack.SAXS.Server.Domain.Entities.Particles;
+using Fullstack.SAXS.Server.Infastructure.Factories;
+using MathNet.Numerics.Distributions;
+using Moq;
+
+namespace Fullstack.SAXS.Application.Tests
+{
+    public class SysServiceTests
+    {
+        private SysService _service;
+
+        [SetUp]
+        public void Setup()
+        {
+            var factoryMock = new Mock<AreaParticleFactory>();
+            var storageMock = new Mock<IStorage>();
+            var graphMock = new Mock<IGraphService>();
+
+            storageMock
+                .Setup(s => s.GetAreaAsync(It.IsAny<Guid>()))
+                .Returns<Guid>(
+                    (id) => Task.FromResult(CreateSomeArea(1000)
+                ));
+
+            graphMock
+                .Setup(g => g.GetHtmlPageAsync(It.IsAny<float[]>(), It.IsAny<float[]>()))
+                .Returns<float[], float[]>((x, y) =>
+                {
+                    var builder = new StringBuilder();
+
+                    for (int i = 0; i < x.Length; i++)
+                    {
+                        builder.AppendLine($"{x[i]} - {y[i]}");
+                    }
+
+                    return Task.FromResult(builder.ToString());
+                });
+
+            _service = new SysService(
+                factoryMock.Object, 
+                storageMock.Object, 
+                graphMock.Object
+            );
+        }
+
+        [Test]
+        public async Task CreatePhiGrafAsync_5Layer_PhiValues()
+        {
+            // Arrange
+            var layersNum = 5;
+
+            // Act
+            var phis = await _service.CreatePhiGrafAsync(Guid.NewGuid(), layersNum);
+
+            // Assert
+            Assert.Pass(phis);
+        }
+
+        private Area CreateSomeArea(int particleNum)
+        {
+            var areaRadius = 3000;
+            var maxParticleSize = 5;
+
+            var area = new SphereArea(0, areaRadius, maxParticleSize);
+
+            area.Fill(InfParticleGenerator(maxParticleSize, areaRadius), particleNum);
+
+            return area;
+        }
+
+        private IEnumerable<Particle> InfParticleGenerator(float maxParticleSize, float areaRadius)
+        {
+            var random = new Random();
+            var gamma = new Gamma(3, 2.5f);
+
+            while (true)
+            {
+                var size = gamma.GetGammaRandom(1, maxParticleSize);
+
+                var a = random.GetEvenlyRandom(-360, 360);
+                var b = random.GetEvenlyRandom(-360, 360);
+                var g = random.GetEvenlyRandom(-360, 360);
+
+                var x = random.GetEvenlyRandom(-areaRadius, areaRadius);
+                var y = random.GetEvenlyRandom(-areaRadius, areaRadius);
+                var z = random.GetEvenlyRandom(-areaRadius, areaRadius);
+
+                yield return new IcosahedronParticle(size, new(x, y, z), new(a, b, g));
+            }
+        }
+    }
+}
---
+++ Fullstack.SAXS.Domain.Tests/IcosahedronParticleTests.cs
@@ -0,0 +1,67 @@
+﻿using Fullstack.SAXS.Server.Domain.Entities.Particles;
+
+namespace Fullstack.SAXS.Domain.Tests;
+
+public class IcosahedronParticleTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Intersect_P1InsideP2_True()
+    {
+        // Arrenge
+        var particle1 = new IcosahedronParticle(1, new(2, 2, 2), new(0, 0, 0));
+        var particle2 = new IcosahedronParticle(3, new(0, 0, 0), new(0, 0, 0));
+
+        // Act
+        var result = particle1.Intersect(particle2);
+
+        // Assert
+        Assert.That(result);
+    }
+
+    [Test]
+    public void Intersect_P2InsideP1_True()
+    {
+        // Arrenge
+        var particle1 = new IcosahedronParticle(3, new (0, 0, 0), new (0, 0, 0));
+        var particle2 = new IcosahedronParticle(1, new(2, 2, 2), new(0, 0, 0));
+
+        // Act
+        var result = particle1.Intersect(particle2);
+
+        // Assert
+        Assert.That(result);
+    }
+
+    [Test]
+    public void Intersect_P2VertexInsideP1_True()
+    {
+        // Arrenge
+        var particle1 = new IcosahedronParticle(3, new(0, 0, 0), new(0, 0, 0));
+        var particle2 = new IcosahedronParticle(1, new(4f, 4f, 2), new(0, 0, 0));
+
+        // Act
+        var result = particle1.Intersect(particle2);
+
+        // Assert
+        Assert.That(result);
+    }
+
+    [Test]
+    public void Intersect_P1DontIntersectWithP2_False()
+    {
+        // Arrenge
+        var particle1 = new IcosahedronParticle(3, new(0, 0, 0), new(0, 0, 0));
+        var particle2 = new IcosahedronParticle(1, new(4.5f, 4.5f, 2), new(0, 0, 0));
+
+        // Act
+        var result = particle1.Intersect(particle2);
+
+        // Assert
+        Assert.That(!result);
+    }
+}
