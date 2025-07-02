5239c6892888d7c8311584693975ddd80aebf160
denis <denisyakubov321@gmail.com>
2025-07-02 23:48:46 +0300

    Added ExceptionHandlingMiddleware

---
+++ Fullstack.SAXS.Server/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+
+namespace Fullstack.SAXS.Server.Middlewares
+{
+    public class ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+    {
+        private readonly RequestDelegate _next = next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger = logger;
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context); // продолжаем конвейер
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Unhandled exception");
+
+                context.Response.StatusCode = 500;
+                context.Response.ContentType = "application/json";
+
+                var errorResponse = new
+                {
+                    error = "Internal Server Error",
+                    message = ex.Message // в проде лучше убрать подробности
+                };
+
+                var json = JsonSerializer.Serialize(errorResponse);
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
