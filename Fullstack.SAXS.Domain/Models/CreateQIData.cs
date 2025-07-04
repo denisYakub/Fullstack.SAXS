115dfb50469518626ab87e5ee633382338da3fdb
denis <denis@PC_DEN4IK>
2025-07-04 14:04:10 +0300

    adding dto

---
+++ Fullstack.SAXS.Domain/Models/CreateQIData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fullstack.SAXS.Domain.Enums;
+
+namespace Fullstack.SAXS.Domain.Models
+{
+    public record CreateQIData(
+        double QMin, double QMax, 
+        int QNum, StepTypes StepType
+    );
+}
---
+++ Fullstack.SAXS.Domain/Models/CreateSysData.cs
@@ -0,0 +1,11 @@
+﻿namespace Fullstack.SAXS.Domain.Models
+{
+    public record CreateSysData(
+        double AreaSize, int AreaNumber, int ParticleNumber,
+        double ParticleMinSize, double ParticleMaxSize,
+        double ParticleSizeShape, double ParticleSizeScale,
+        double ParticleAlphaRotation,
+        double ParticleBetaRotation,
+        double ParticleGammaRotation
+    );
+}
