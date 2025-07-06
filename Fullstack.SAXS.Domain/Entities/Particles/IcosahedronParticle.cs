71f9d33b53983ab05a5c6972d872fbe1a6355925
denis <denis@PC_DEN4IK>
2025-07-06 13:17:23 +0300

    Adjusting arrayes stiles

--- Fullstack.SAXS.Domain/Entities/Particles/IcosahedronParticle.cs
+++ Fullstack.SAXS.Domain/Entities/Particles/IcosahedronParticle.cs
@@ -24,26 +24,16 @@ namespace Fullstack.SAXS.Domain.Entities.Particles
         private Vector3D[] _vertices;
 
         private static readonly int[][] _faces = [
-            [0, 11, 5],
-            [0, 5, 1],
-            [0, 1, 7],
-            [0, 7, 10],
-            [0, 10, 11],
-            [1, 5, 9],
-            [5, 11, 4],
-            [11, 10, 2],
-            [10, 7, 6],
-            [7, 1, 8],
-            [3, 9, 4],
-            [3, 4, 2],
-            [3, 2, 6],
-            [3, 6, 8],
-            [3, 8, 9],
-            [4, 9, 5],
-            [2, 4, 11],
-            [6, 2, 10],
-            [8, 6, 7],
-            [9, 8, 1]
+            [0, 11, 5], [0, 5, 1],
+            [0, 1, 7], [0, 7, 10],
+            [0, 10, 11], [1, 5, 9],
+            [5, 11, 4], [11, 10, 2],
+            [10, 7, 6], [7, 1, 8],
+            [3, 9, 4], [3, 4, 2],
+            [3, 2, 6], [3, 6, 8],
+            [3, 8, 9], [4, 9, 5],
+            [2, 4, 11], [6, 2, 10],
+            [8, 6, 7], [9, 8, 1]
         ];
         private static double _phi = (1 + Math.Sqrt(5)) / 2;
         public IcosahedronParticle(double size, Vector3D center, EulerAngles rotationAngles)
