1ec628165ea6f6cc851d76558b1cc11caa59efe7
denis <denis@PC_DEN4IK>
2025-07-08 14:02:52 +0300

    Added static baseVertices to all particles

---
+++ Fullstack.SAXS.Domain.Tests/C60Tests.cs
@@ -0,0 +1,25 @@
+﻿using Fullstack.SAXS.Domain.Entities.Particles;
+
+namespace Fullstack.SAXS.Domain.Tests;
+
+public class C60Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Size_OuterBiggerThenInnerRadius_True()
+    {
+        // Arrange
+        var c60 = new C60(5, new(0, 0, 0), new(0, 0, 0));
+
+        // Act
+        var innerR = c60.InnerSphereRadius;
+        var outerR = c60.OuterSphereRadius;
+
+        //Assert
+        Assert.That(innerR, Is.LessThan(outerR));
+    }
+}
