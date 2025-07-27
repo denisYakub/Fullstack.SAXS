576e9de70d07406bd05aaea2b315318c6b3221e8
denis <denis@PC_DEN4IK>
2025-07-27 12:55:51 +0300

    Added Application modul tests project. SysService.CreateQs was tested.

---
+++ Fullstack.SAXS.Application.Tests/SysServiceTests.cs
@@ -0,0 +1,53 @@
+﻿using Fullstack.SAXS.Application.Services;
+using Fullstack.SAXS.Domain.Enums;
+
+namespace Fullstack.SAXS.Application.Tests
+{
+    public class SysServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CreateQs_Linear_Pass()
+        {
+            // Arrange
+            var qMin = 0.02;
+            var qMax = 5.0;
+            var qNum = 150;
+
+            // Act
+            var qVector = SysService.CreateQs(qMin, qMax, qNum);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(qVector, Has.Length.EqualTo(qNum));
+                Assert.That(qVector.First(), Is.AtLeast(qMin));
+                Assert.That(qVector.Last(), Is.AtMost(qMax));
+            });
+        }
+
+        [Test]
+        public void CreateQs_Logarithmic_Pass()
+        {
+            // Arrange
+            var qMin = 0.02;
+            var qMax = 5.0;
+            var qNum = 150;
+
+            // Act
+            var qVector = SysService.CreateQs(qMin, qMax, qNum, StepTypes.Logarithmic);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(qVector, Has.Length.EqualTo(qNum));
+                Assert.That(qVector.First(), Is.AtLeast(qMin));
+                Assert.That(qVector.Last(), Is.AtMost(qMax));
+            });
+        }
+    }
+}
